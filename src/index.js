2ef8b2bf379435dc8fab4a3322669f3b310785d7
pedrojuan <peter.damondamon@davao.sti.edu>
2018-11-16 23:50:28 +0800

    done random walker in p5

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ let inst;
 function setup(){
   createCanvas(displayWidth, displayHeight);
   inst = new inobj(displayWidth/2, displayHeight/2)
+  background(221);
 }
 function draw(){
-  background(221);
+
   inst.display();
+  inst.update();
 }
--- src/inobj.js
+++ src/inobj.js
@@ -1,11 +1,26 @@
 function inobj(x, y){
   this.post = createVector(x,y);
   picturesize = 20;
-
+  this.speed = createVector(1,2);
 }
 
 inobj.prototype.display = function() {
   stroke(150,50,65);
   noFill();
   ellipse(this.post.x, this.post.y, picturesize, picturesize);
+}
+inobj.prototype.update = function() {
+
+  const choice = Math.round(randomGaussian(3));
+  if(choice == 0){
+    this.post.x = this.post.x + this.speed.x;
+  }else  if(choice == 1) {
+    this.post.y = this.post.y + this.speed.y;
+  }else  if(choice == 2 ){
+    this.post.x = this.post.x + -1 * this.speed.x;
+  }else  if(choice == 3){
+    this.post.y = this.post.y + -1 * this.speed.y;
+  }else{
+    this.post.add(this.speed)
+  }
 }
