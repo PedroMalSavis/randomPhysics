3603c29f5b530f050890d1c336f3e64e4da12c20
pedrojuan <peter.damondamon@davao.sti.edu>
2018-11-16 23:17:30 +0800

    ni drawing na jud og lingin

--- src/inobj.js
+++ src/inobj.js
@@ -1,10 +1,11 @@
 function inobj(x, y){
-  const post = createVector(x,y);
+  this.post = createVector(x,y);
   picturesize = 20;
 
 }
 
 inobj.prototype.display = function() {
   stroke(150,50,65);
-  ellipse(this.post.x, this.post.y, 20, 20);
+  noFill();
+  ellipse(this.post.x, this.post.y, picturesize, picturesize);
 }
